c4f78be91514a2af565aff197909e11c49965146
khadkaabhishek07 <khadkaabhishek749@gmail.com>
2024-12-01 09:53:34 +0545

    Week 5 Commit

--- Todo/Models/User.cs
+++ Todo/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,22 @@ namespace Todo.Models
 {
     public class User
     {
+        [Key]
         public int UserId { get; set; }
+
         public string UserName { get; set; }
+
         public string Password { get; set; }
+
         public string Email { get; set; }
+
         public string Name { get; set; }
+
         public string Address { get; set; }
-        public string Gender { get; set; }
+
+        public string Gender { get; set; } 
+
         public string Phone { get; set; }
     }
+   
 }
